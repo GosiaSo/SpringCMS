baf986f9ccafa3762cf09da587365050a5f02743
malgorzatasobocinska <sobocinska.malgo@gmail.com>
2020-08-17 19:25:09 +0200

    dao

---
+++ src/main/java/pl/coderslab/dao/ArticleDao.java
@@ -0,0 +1,28 @@
+package pl.coderslab.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.model.Article;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
+public class ArticleDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void save(Article article) {
+        entityManager.persist(article);
+    }
+
+    public void delete(Article article) {
+        entityManager.remove(entityManager.contains(article) ? article : entityManager.merge(article));
+    }
+
+    public void update(Article article) {
+        entityManager.merge(article);
+    }
+}
---
+++ src/main/java/pl/coderslab/dao/CategoryDao.java
@@ -0,0 +1,28 @@
+package pl.coderslab.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.model.Category;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
+public class CategoryDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void save(Category category) {
+        entityManager.persist(category);
+    }
+
+    public void delete(Category category){
+        entityManager.remove(entityManager.contains(category) ? category : entityManager.merge(category));
+    }
+
+    public void update(Category category){
+        entityManager.merge(category);
+    }
+}
