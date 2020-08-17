08ee6fbfd70ab190028da403c9133a66a05401c6
malgorzatasobocinska <sobocinska.malgo@gmail.com>
2020-08-17 19:21:31 +0200

    getters & setters

--- src/main/java/pl/coderslab/model/Category.java
+++ src/main/java/pl/coderslab/model/Category.java
@@ -11,4 +11,31 @@ public class Category {
     @Column(length = 100)
     private String name;
     private String description;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
